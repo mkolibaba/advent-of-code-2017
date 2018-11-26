1a20220f73dbb0f395fa2781fdff2953c5e10aa4
Maksim Kolibaba <maksim.kolibaba@accenture.com>
2018-11-26 11:11:20 +0300

    Solve 4.1

---
+++ Program.fs
@@ -0,0 +1,42 @@
+﻿module aoc
+
+open System
+open System.IO
+
+module Utils =
+    let readInput day = (sprintf "resources\\input-%s.txt" day) |> File.ReadLines |> List.ofSeq
+    
+    let splitBy (ch: string) (str: string) = str.Split([|ch|], StringSplitOptions.None) |> List.ofArray
+
+module Day4 =
+    let solve1 (input: string list) =
+        let (||>>) (l: string list) f = (l, l.Length) |> f
+
+        input
+        |> List.sumBy (fun s -> 
+            Utils.splitBy " " s
+            ||>> fun (t, l) -> if (t |> List.distinct |> List.length = l) then 1 else 0)
+            
+    let solve2 (input: string list) =
+        0 
+
+    let decide part =
+        match part with
+        | "2" -> solve2
+        | "1" | _ -> solve1
+
+[<EntryPoint>]
+let main argv =
+    let day = argv |> Array.head
+    let part = argv |> Array.tail |> Array.head
+
+    let decider = 
+        match day with
+        | "4" -> Day4.decide
+        | _ -> failwith "wrong day"
+    
+    let solver = decider part
+
+    day |> Utils.readInput |> solver |> printfn "%A"
+
+    0
